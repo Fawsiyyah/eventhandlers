8c26401d0a976dfaf55ef0da8640712e46f88d90
fawsiyyah <fawsiyyahlamidi@gmail.com>
2023-04-10 12:15:07 +0100

    New commit

---
+++ src/components/profiles.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+
+const Profile = () => {
+    return (
+        <main>
+            <My>
+                <h1>My name is Fawsiyyah</h1>
+            </My>
+        </main>
+    )
+}
+export default Profile
---
+++ src/components/search.jsx
@@ -0,0 +1,16 @@
+import "../App.css";
+import Profile from "./profiles"
+
+const Search = () => {
+    return (
+        <div>
+            <form action="">
+                <input type="search" placeHolder="Search by name..." name="search" id="" />
+
+                <Profile />
+            </form>
+            
+        </div>
+    );
+}
+export default Search;
